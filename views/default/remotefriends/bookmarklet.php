d2a0f825ec63150edbcf83692299d828e3cc2da5
Marcus Povey <marcus@marcus-povey.co.uk>
2014-05-25 17:19:24 +0100

    Bookmarklet code

---
+++ views/default/remotefriends/bookmarklet.php
@@ -0,0 +1,1 @@
+<a href="javascript:(function(){location.href='<?= elgg_get_site_url() ?>remotefriends/findfriends/?u='+encodeURIComponent(location.href);})();">Add as friend on <?= elgg_get_site_entity()->name ?>...</a>
