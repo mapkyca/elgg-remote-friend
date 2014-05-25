a005c20f1b5199e259920efe2fd66e2f3866aead
Marcus Povey <marcus@marcus-povey.co.uk>
2014-05-25 17:55:29 +0100

    Get by uuid

--- lib/remote-friends.php
+++ lib/remote-friends.php
@@ -31,3 +31,45 @@ function removeDuplicateProfiles(array $hcards) {
 
     return $cards;
 }
+
+/**
+ * Return either a local or remote user based on it's UUID
+ */
+function get_user_by_profile_UUID($url) {
+    $user = false;
+    
+    // Try and user by profile if local
+    if (is_local_uuid($url)) {
+	
+	if (preg_match("/".str_replace('/','\/', $CONFIG->wwwroot)."profile\/([0-9a-zA-Z]+)\/?/", $url, $match))
+		$username = $match[1];
+	
+	$user = get_user_by_username($username);
+    }
+    
+    // otherwise see if the profile ID has been attached to a user
+    if (!$user) {
+	if ($entities = elgg_get_entities_from_metadata([
+	    'type' => 'user',
+	    'metadata_name_value_pairs' => ['name' => 'uuid', 'value' => $url]
+	])) {
+	    error_log("*** Found a user $url as remote");
+	    $user = $entities[0];
+	}
+    }
+    
+    return $user;
+}
+
+/**
+ * Return whether the uuid is a local address.
+ */
+function is_local_uuid($uuid) {
+    if (($uuid_parse = parse_url($uuid)) && ($url_parse = parse_url(elgg_get_site_url()))) {
+	if ($uuid_parse['host'] == $url_parse['host']) {
+	    return true;
+	}
+    }
+
+    return false;
+}
