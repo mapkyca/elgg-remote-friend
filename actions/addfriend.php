f60481b7b237425c676b42099338db12c1a1cc6b
Marcus Povey <marcus@marcus-povey.co.uk>
2014-05-26 18:26:59 +0100

    Updating with changes

--- actions/addfriend.php
+++ actions/addfriend.php
@@ -8,11 +8,11 @@ $user = elgg_get_logged_in_user_entity();
 
 if ($uuid = get_input('uuid')) {
     
-    if ($new_user = get_user_by_profile_UUID($uuid))
+    if (!$new_user = get_user_by_profile_UUID($uuid))
     {
 	// No user found, so create it if it's remote
 	if (!is_local_uuid($uuid)) {
-	    error_log("Creating new remote user");
+	    error_log("*** Creating new remote user");
 
 	    $new_user = new ElggUser();
 	    $new_user->subtype = 'remote_user';
@@ -37,22 +37,24 @@ if ($uuid = get_input('uuid')) {
 	else
 	    register_error("Sorry, local users can't be created, but you shouldn't see this message!");
 	
-	if ($new_user) {
-		    
-	    error_log("Trying a follow");
-
-	    if ($user->addFriend($new_user)) {
-
-		error_log("User added to following");
-		
-		system_message("You are now following " . $new_user->name);
-	    } else
-		error_log('Could not follow user for some reason (probably already following)');
-	} else
-	    register_error('Sorry, that user doesn\'t exist!');
     } else
-	error_log("New user found as " . $new_user->uuid);
+	error_log("*** New user found as " . $new_user->uuid);
     
+    if ($new_user) {
+		    
+	error_log("**** Trying a follow");
+
+	if ($user->addFriend($new_user->guid)) {
+
+	    error_log("*** User added to following");
+
+	    system_message("You are now following " . $new_user->name);
+	    
+	    forward('friends/' . elgg_get_logged_in_user_entity()->username);
+	} else
+	    register_error('Could not follow user for some reason (probably already following)');
+    } else
+	register_error('Sorry, that user doesn\'t exist!');
 }
 else 
     register_error("No UUID, please try that again!");
